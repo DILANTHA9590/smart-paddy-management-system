69d60a0c709e4c64dc04f26f3a1f916a25e243f5
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-06-12 21:59:52 +0530

    create api  property for role dto

--- src/api/roles/dto/create-role.dto.ts
+++ src/api/roles/dto/create-role.dto.ts
@@ -1,10 +1,20 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { UserRole } from '../entities/role.enum';
 
 export class CreateRoleDto {
+  @ApiProperty({
+    enum: UserRole,
+    example: UserRole.FARMER,
+    description: 'Role name',
+  })
   @IsEnum(UserRole)
   roleName!: UserRole;
 
+  @ApiPropertyOptional({
+    example: 'Farmer role with agriculture permissions',
+    description: 'Role description',
+  })
   @IsOptional()
   @IsString()
   description?: string;
