188052a0c4c23c7cb9e1a41875b9f092cbda9492
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-06-12 21:27:52 +0530

    implement RoleController with complete CRUD endpoints

--- src/api/roles/roles.controller.ts
+++ src/api/roles/roles.controller.ts
@@ -1,34 +1,36 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Req, Query } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { UpdateRoleDto } from './dto/update-role.dto';
+import { ApiResponseDto } from 'src/common/dto/api-respose-dto';
+import { Role } from './entities/role.entity';
 
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
   @Post()
-  create(@Body() createRoleDto: CreateRoleDto) {
-    return this.rolesService.create(createRoleDto);
+  create(@Body() createRoleDto: CreateRoleDto, @Req() req:any):Promise<ApiResponseDto<null>>{
+    return this.rolesService.create(createRoleDto,req.user);
   }
 
   @Get()
-  findAll() {
-    return this.rolesService.findAll();
+  findAll(@Query('name') name:string):Promise<ApiResponseDto<Role[]>> {
+    return this.rolesService.findAll(name);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rolesService.findOne(+id);
+  findOne(@Param('id') id: string):Promise<ApiResponseDto<Role>> {
+    return this.rolesService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRoleDto: UpdateRoleDto) {
-    return this.rolesService.update(+id, updateRoleDto);
+  update(@Param('id') id:string, @Body() updateRoleDto: UpdateRoleDto):Promise<ApiResponseDto<Role>> {
+    return this.rolesService.updateRole(id, updateRoleDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.rolesService.remove(+id);
+  remove(@Param('id') id: string):Promise<ApiResponseDto<null>> {
+    return this.rolesService.remove(id);
   }
 }
