d47c4f1d163248fbfe1d3579f725a4d566223442
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-06-22 23:13:19 +0530

    add OTP service for generating, storing, and sending OTP codes

--- src/api/otp/otp.service.ts
+++ src/api/otp/otp.service.ts
@@ -1,26 +1,44 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateOtpDto } from './dto/create-otp.dto';
 import { UpdateOtpDto } from './dto/update-otp.dto';
 import { RedisService } from '../redis/redis.service';
+import { EmailService } from '../email/email.service';
+import { ApiResponseDto } from 'src/common/dto/api-respose-dto';
 
 @Injectable()
 export class OtpService {
   constructor(
-    private readonly rediseService:RedisService //redis service
+    private readonly rediseService:RedisService, //redis service
+    private readonly emailService:EmailService
     
   ){}
- async create(createOtpDto: CreateOtpDto) {
-    const {email}= createOtpDto
-    const otp = String( Math.floor(100000 + Math.random() * 900000))//genarate random no
+ async create(email:string):Promise<ApiResponseDto<null>> {
+
+  const otp = this.genarateOtp() 
     const existingValue = await this.rediseService.getRedis(email)
     if(existingValue)  throw new ConflictException("We Are Already Send Otp Your Email")
-    await this.rediseService.setRedis(email,otp)  
-
-
-
-    
+      try {
+            await this.rediseService.setRedis(email,otp)  
+            await this.emailService.sendOtp(email,otp)
+            return {
+    success: true,
+    message: "Email send succsessfully ",
+    data:null
+ }
+       
+      } catch (error) {
+          console.error('Send OTP failed:', error);
+           throw new InternalServerErrorException('Failed to send OTP email');
+      }
 
   
+  }
+
+
+
+  genarateOtp(){
+     const otp = String( Math.floor(100000 + Math.random() * 900000))//genarate random no
+     return otp
   }
 
 
