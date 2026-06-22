446d42c86ca54f4889bd56b59e910d00f33a9444
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-06-22 10:43:39 +0530

    create dto for otp  email validation 🎉🌟

--- src/api/otp/dto/create-otp.dto.ts
+++ src/api/otp/dto/create-otp.dto.ts
@@ -1,1 +1,8 @@
-export class CreateOtpDto {}
+import { IsEmail, IsNotEmpty, IsString, Length } from "class-validator";
+
+export class CreateOtpDto {
+  @IsEmail({}, { message: 'Please enter a valid email address' })
+  @IsNotEmpty({ message: 'Email cannot be empty' })
+  email!: string;
+
+}
