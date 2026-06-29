a27471b0a0f0e7ce3aa2f6d475568a7bd11d6a85
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-06-29 23:21:13 +0530

    implent faremr ase meber entity

---
+++ src/api/farmer-association/entities/farmers-association-member.entity.ts
@@ -0,0 +1,63 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+  Unique,
+} from 'typeorm';
+import { FarmersAssociation } from './farmer-association.entity';
+import { Farmer } from 'src/api/farmers/entities/farmer.entity';
+
+
+@Entity('farmers_association_members')
+@Unique('UQ_FARMER_ASSOCIATION_MEMBER', ['farmer', 'association'])
+
+export class FarmersAssociationMember {
+  @PrimaryGeneratedColumn('uuid')
+  id!: string;
+
+
+@ManyToOne(
+  () => Farmer,
+  (farmer) => farmer.associationMemberships,
+  {
+    onDelete: 'CASCADE',
+  },
+)
+@JoinColumn({ name: 'farmerId'})
+farmer?: Farmer;
+
+@ManyToOne(
+  () => FarmersAssociation,
+  (association) => association.members,
+  {
+    onDelete: 'CASCADE',
+  },
+)
+@JoinColumn({ name: 'associationId' })
+association?: FarmersAssociation;
+
+
+
+//   @Column({
+//     type: 'enum',
+//     enum: FarmersAssociationMemberStatus,
+//     default: FarmersAssociationMemberStatus.ACTIVE,
+//   })
+//   status: FarmersAssociationMemberStatus;
+
+  @Column({
+    type: 'timestamp',
+    nullable: true,
+  })
+  joinedAt?: Date;
+
+  @CreateDateColumn()
+  createdAt!: Date;
+
+  @UpdateDateColumn()
+  updatedAt!: Date;
+}
