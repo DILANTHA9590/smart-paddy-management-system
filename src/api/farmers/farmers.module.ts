85c7b8e69d81a9ad21f371412422e8be8eb55f83
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-06-30 15:45:32 +0530

    import user model

--- src/api/farmers/farmers.module.ts
+++ src/api/farmers/farmers.module.ts
@@ -3,9 +3,10 @@ import { FarmersService } from './farmers.service';
 import { FarmersController } from './farmers.controller';
 import { Farmer } from './entities/farmer.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entities/user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Farmer])],
+  imports: [TypeOrmModule.forFeature([Farmer,User])],
   controllers: [FarmersController],
   providers: [FarmersService],
 })
