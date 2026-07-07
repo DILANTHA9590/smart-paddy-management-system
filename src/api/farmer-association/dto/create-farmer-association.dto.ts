f4057fb2d247938344aa48c7b83f4e144e2b4827
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-07-07 19:22:38 +0530

    create  farmer association dto ❇️

--- src/api/farmer-association/dto/create-farmer-association.dto.ts
+++ src/api/farmer-association/dto/create-farmer-association.dto.ts
@@ -1,1 +1,38 @@
-export class CreateFarmerAssociationDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  Length,
+} from 'class-validator';
+
+export class CreateFarmersAssociationDto {
+  @ApiProperty({
+    example: 'Kandy Farmers Association',
+    description: 'Unique name of the farmers association',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Length(3, 150)
+  name!: string;
+  
+@IsString()
+@IsOptional()
+  associationCode!: string;
+  @ApiProperty({
+    example: 'Kandy',
+    description: 'District where the farmers association is located',
+  })
+  @IsString()
+  @IsNotEmpty()
+  district!: string;
+
+  @ApiProperty({
+    example: 'Peradeniya',
+    description: 'Village where the farmers association is located',
+  })
+  @IsString()
+  @IsNotEmpty()
+  @Length(2, 100)
+  village!: string;
+}
