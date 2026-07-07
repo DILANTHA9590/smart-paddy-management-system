aa83f8c8e00e3e0d32dc0d30bb8076e5910aad44
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-07-07 20:09:44 +0530

    fix import issue

--- src/api/farmer-association/dto/update-farmer-association.dto.ts
+++ src/api/farmer-association/dto/update-farmer-association.dto.ts
@@ -1,4 +1,5 @@
 import { PartialType } from '@nestjs/swagger';
-import { CreateFarmerAssociationDto } from './create-farmer-association.dto';
+import { CreateFarmersAssociationDto } from './create-farmer-association.dto';
 
-export class UpdateFarmerAssociationDto extends PartialType(CreateFarmerAssociationDto) {}
+
+export class UpdateFarmerAssociationDto extends PartialType(CreateFarmersAssociationDto) {}
