5d4715b8a4fe58f61d3665079d334df409ae91f4
dilanthanayanajith <dilanthanayanajith@gmail.com>
2026-07-08 19:44:32 +0530

    register famer association module entity

--- src/api/farmer-association/farmer-association.module.ts
+++ src/api/farmer-association/farmer-association.module.ts
@@ -4,9 +4,11 @@ import { FarmerAssociationController } from './farmer-association.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { FarmersAssociation } from './entities/farmer-association.entity';
 import { Farmer } from '../farmers/entities/farmer.entity';
+import { FarmersAssociationNotice } from './entities/farmers-association-notice.entity';
+import { FarmersAssociationMember } from './entities/farmers-association-member.entity';
 
 @Module({
-    imports: [TypeOrmModule.forFeature([FarmersAssociation,Farmer]),],
+    imports: [TypeOrmModule.forFeature([FarmersAssociation,Farmer,FarmersAssociationNotice,FarmersAssociationMember]),],
   controllers: [FarmerAssociationController],
   providers: [FarmerAssociationService],
 
